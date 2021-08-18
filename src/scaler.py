dc84af71fdda499d7362531857c3f8876c2f785b
Felix Burkhardt <fxburk@gmail.com>
2021-08-18 13:21:23 +0200

    added speaker-dependent scaling

--- src/scaler.py
+++ src/scaler.py
@@ -9,6 +9,7 @@ class Scaler:
     # class to normalize speech parameters
 
     def __init__(self, config, train_data_df, test_data_df, train_feats, test_feats):
+        self.config = config
         self.util = Util(config)
         scaler_type = config['FEATS']['scale'] 
         if scaler_type == 'standard':
@@ -24,6 +25,16 @@ class Scaler:
         self.data_test = test_data_df
 
     def scale(self):
+        try:
+            scale_speakers = bool(self.config['FEATS']['scale_speakers'])
+        except KeyError:
+            scale_speakers = False
+        if not scale_speakers:
+            return self.scale_all()
+        else:
+            return self.speaker_scale()
+
+    def scale_all(self):
         self.scaler.fit(self.feats_train.values)
         self.feats_train = self.scale_df(self.feats_train)
         self.feats_test = self.scale_df(self.feats_test)
@@ -33,3 +44,15 @@ class Scaler:
         scaled_features = self.scaler.fit_transform(df.values)
         df = pd.DataFrame(scaled_features, index=df.index, columns=df.columns)
         return df 
+
+    def speaker_scale(self):
+        self.feats_train = self.speaker_scale_df(self.data_train, self.feats_train)
+        self.feats_test = self.speaker_scale_df(self.data_test, self.feats_test)
+        return self.feats_train, self.feats_test
+
+
+    def speaker_scale_df(self, df, feats_df):
+        for speaker in df['speaker'].unique():
+            indices = df.loc[df['speaker'] == speaker].index
+            feats_df.loc[indices, :] = self.scaler.fit_transform(feats_df.loc[indices, :]) 
+        return feats_df
