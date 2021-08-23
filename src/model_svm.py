fdd038a689930a7f6be9d60c25282ec0d84677fe
Felix Burkhardt <fburkhardt@audeering.com>
2021-08-23 12:50:01 +0200

    added parameter optimization

--- src/model_svm.py
+++ src/model_svm.py
@@ -7,13 +7,14 @@ from model import Model
 import glob_conf
 from reporter import Reporter
 from result import Result
+from sklearn.model_selection import GridSearchCV
+import ast
 
 class SVM_model(Model):
     """An SVM model"""
     def __init__(self, df_train, df_test, feats_train, feats_test):
         super().__init__(df_train, df_test, feats_train, feats_test)
-        c = float(self.util.config_val('MODEL', 'C', '0.001'))
-        self.clf = svm.SVC(kernel='linear', C=c) # set up the classifier
+        self.clf = svm.SVC(kernel='linear') # set up the classifier
 
     def set_C(self, c):
         """Set the C parameter"""
@@ -23,9 +24,24 @@ class SVM_model(Model):
         """Train the model"""
         target = glob_conf.config['DATA']['target']
         if self.feats_train.df.isna().to_numpy().any():
-            self.util.error('NAN')
+            self.util.error('NANs exist')
         feats = self.feats_train.df.to_numpy()
-        self.clf.fit(feats, self.df_train[target])
+        try:
+            # tune the model meta parameters
+            tuning_params = ast.literal_eval(glob_conf.config['MODEL']['tuning_params'])
+            tuned_params={}
+            scoring = glob_conf.config['MODEL']['scoring']
+            for param in tuning_params:
+                values = ast.literal_eval(glob_conf.config['MODEL'][param])
+                tuned_params[param] = values
+            self.util.debug(f'tuning on {tuned_params}')
+            self.clf = GridSearchCV(self.clf, tuned_params, refit = True, verbose = 3, scoring=scoring)
+            self.clf.fit(feats, self.df_train[target])
+        except KeyError:
+            c = float(self.util.config_val('MODEL', 'C', '0.001'))
+            self.set_C(c)
+            self.clf.fit(feats, self.df_train[target])
+        
 
     def predict(self):
         """Predict the whole eval feature set"""
