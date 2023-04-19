43f6b8f5b5c5364da4140ba14bccb249ceb62ec8
FBurkhardt <fburkhardt@audeering.com>
2023-04-19 16:23:51 +0200

    update

--- test_runs.sh
+++ test_runs.sh
@@ -2,6 +2,7 @@ python -m nkululeko.explore --config tests/exp_emodb_explore_data.ini
 python -m nkululeko.explore --config tests/exp_emodb_explore_tsne.ini
 python -m nkululeko.explore --config tests/exp_emodb_explore_features.ini
 python -m nkululeko.nkululeko --config tests/exp_emodb_os_praat_xgb.ini
+python -m nkululeko.nkululeko --config tests/exp_emodb_audmodel_xgb.ini
 python -m nkululeko.nkululeko --config tests/exp_emodb_os_mlp.ini
 python -m nkululeko.nkululeko --config tests/exp_agedb_os_xgr.ini 
 python -m nkululeko.nkululeko --config tests/exp_agedb_os_mlp.ini 
