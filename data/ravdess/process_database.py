2ddd6d3b3f65ea5318d94223ffa2f9300286e1ab
FBurkhardt <fburkhardt@audeering.com>
2023-04-25 16:12:33 +0200

    update

--- data/ravdess/process_database.py
+++ data/ravdess/process_database.py
@@ -9,6 +9,8 @@ Ravdess = './'
 ravdess_directory_list = os.listdir(Ravdess)
 
 file_emotion = []
+file_speaker = []
+file_gender = []
 file_path = []
 for dir in ravdess_directory_list:
     # as their are 20 different actors in our previous directory we need to extract files for each actor.
@@ -19,14 +21,16 @@ for dir in ravdess_directory_list:
             part = part.split('-')
             # third part in each file represents the emotion associated to that file.
             file_emotion.append(int(part[2]))
+            file_speaker.append(int(part[6]))
             file_path.append(Ravdess + dir + '/' + file)
         
 # dataframe for emotion of files
-emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
+#emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
+data_df = pd.DataFrame([file_emotion,file_speaker] columns=['emotion', 'speaker'])
 
 # dataframe for path of files.
 path_df = pd.DataFrame(file_path, columns=['file'])
-Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
+Ravdess_df = pd.concat([data_df, path_df], axis=1)
 
 # changing integers to actual emotions.
 Ravdess_df.emotion.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
