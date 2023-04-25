a2ed9e817aadda2e1ba845c2cdbdcea2c450672f
FBurkhardt <fburkhardt@audeering.com>
2023-04-25 14:19:51 +0200

    update

--- data/ravdess/process_database.py
+++ data/ravdess/process_database.py
@@ -22,13 +22,14 @@ for dir in ravdess_directory_list:
             file_path.append(Ravdess + dir + '/' + file)
         
 # dataframe for emotion of files
-emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
 
 # dataframe for path of files.
-path_df = pd.DataFrame(file_path, columns=['Path'])
+path_df = pd.DataFrame(file_path, columns=['file'])
 Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
 
 # changing integers to actual emotions.
-Ravdess_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
-Ravdess_df.head()
+Ravdess_df.emotion.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
+Ravdess_df = Ravdess_df.set_index('file')
+print(Ravdess_df.head())
 Ravdess_df.to_csv('ravdess.csv')
