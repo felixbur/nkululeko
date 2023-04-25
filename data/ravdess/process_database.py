90b1be6605761ec225246f30623d47cfc67a1e6d
FBurkhardt <fburkhardt@audeering.com>
2023-04-25 16:21:29 +0200

    update

--- data/ravdess/process_database.py
+++ data/ravdess/process_database.py
@@ -28,14 +28,23 @@ for dir in ravdess_directory_list:
         
 # dataframe for emotion of files
 #emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
-data_df = pd.DataFrame(list(zip(file_emotion,file_speaker)), columns=['emotion', 'speaker'])
+data_df = pd.DataFrame(list(zip(file_emotion,file_speaker,file_gender)), columns=['emotion', 'speaker', 'gender'])
 
 # dataframe for path of files.
 path_df = pd.DataFrame(file_path, columns=['file'])
-Ravdess_df = pd.concat([data_df, path_df], axis=1)
+result_df = pd.concat([data_df, path_df], axis=1)
 
 # changing integers to actual emotions.
-Ravdess_df.emotion.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
-Ravdess_df = Ravdess_df.set_index('file')
-print(Ravdess_df.head())
-Ravdess_df.to_csv(database_name+'.csv')
+result_df.emotion.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
+# changing numbers to gender designations
+def get_gender(x):
+    if x%2==0:
+        return 'female'
+    else:
+        return 'male'
+result_df.gender = result_df.gender.map(lambda x: get_gender(x))
+
+
+result_df = result_df.set_index('file')
+print(result_df.head())
+result_df.to_csv(database_name+'.csv')
