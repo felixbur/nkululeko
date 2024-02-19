994e85084c347aac2b36fa3b99107ec82f267b27
Bagus Tris Atmaja <bagustris@yahoo.com>
2024-02-19 13:41:42 +0900

    add preprocessing for odyssey challenge

---
+++ data/odyssey-cat-2024/process_database.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import argparse
+
+# Use this code to create a .csv file with the necessary format needed for 
+# categorical emotion recognition model
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, default='/data/Odyssey_SER_Challenge/')
+args = parser.parse_args()
+
+# Load Original label_consensus.csv file provided with dataset
+df = pd.read_csv(args.data_dir + 'Labels/labels_consensus.csv')
+
+# Define the emotions
+emotions = ["Angry", "Sad", "Happy", "Surprise", "Fear", "Disgust", "Contempt", "Neutral"]
+emotion_codes = ["A", "S", "H", "U", "F", "D", "C", "N"]
+
+# Create a dictionary for one-hot encoding
+one_hot_dict = {e: [1.0 if e == ec else 0.0 for ec in emotion_codes] for e in emotion_codes}
+
+# Filter out rows with undefined EmoClass
+df = df[df['EmoClass'].isin(emotion_codes)]
+
+# Apply one-hot encoding
+for i, e in enumerate(emotion_codes):
+    df[emotions[i]] = df['EmoClass'].apply(lambda x: one_hot_dict[x][i])
+
+# Select relevant columns for the new CSV
+df_final = df[['FileName', *emotions, 'Split_Set']]
+
+df_train = df_final[df_final.Split_Set == 'Train']
+df_train.to_csv('odyssey_train.csv', index=False)
+
+df_dev = df_final[df_final.Split_Set == 'Development']
+df_dev.to_csv('odyssey_dev.csv', index=False)
+
+# create test partition from partitions.txt
+df_test = pd.read_csv(args.data_dir + 'Partitions.txt', sep='; ', header=None)
+df_test = df_test[df_test.iloc[:, 0] == 'Test3']
+df_test.columns = ['Split_Set', 'FileName']
+df_test.to_csv('odyssey_test.csv', columns=['FileName', 'Split_Set'], index=False)
+
+# Save the processed data to a new CSV file
+# df_final.to_csv('processed_labels.csv', index=False)
+
+print("Processing complete. New file saved as 'odyssey_<partition>.csv'")
