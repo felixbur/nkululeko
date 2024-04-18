76777d427ea3fa08363a4b37c5ca1ae87ee71735
Bagus Tris Atmaja <bagustris@outlook.com>
2024-04-18 19:25:57 +0900

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 
@@ -75,7 +76,7 @@ master_doc = 'index'
 # html_theme_options = {
 
 #     # Set the name of the project to appear in the navigation.
-'nav_title': 'Nkululeko',
+# 'nav_title': 'Nkululeko',
 
 #     # Set the color and the accent color
 #     'color_primary': 'blue',
