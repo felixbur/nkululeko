1d39e87a77373adbdcec0a68f2a0bc6d820f2c27
FBurkhardt <fburkhardt@audeering.com>
2024-04-23 19:00:56 +0200

    update

--- meta/demos/multiple_exeriments/do_experiments.py
+++ meta/demos/multiple_exeriments/do_experiments.py
@@ -1,48 +1,35 @@
 import os
 
-src_path = 'demos/multiple_exeriments/'
 
 classifiers = [
-    {'--model': 'mlp',
-    '--layers': '\"{\'l1\':64,\'l2\':16}\"'},
-    {'--model': 'mlp',
-    '--layers': '\"{\'l1\':64,\'l2\':16}\"',
-    '--learning_rate': '.1',},
-    {'--model': 'mlp',
-    '--layers': '\"{\'l1\':64,\'l2\':16}\"',
-    '--learning_rate': '.0001',
-    '--drop': '.3',
+    {"--model": "mlp", "--layers": "\"{'l1':64,'l2':16}\"", "--epochs": 100},
+    {
+        "--model": "mlp",
+        "--layers": "\"{'l1':128,'l2':64,'l3':16}\"",
+        "--learning_rate": ".01",
+        "--drop": ".3",
+        "--epochs": 100,
     },
-    {'--model': 'xgb',
-    '--epochs':1},
-    {'--model': 'svm',
-    '--epochs':1},
+    {"--model": "xgb", "--epochs": 1},
+    {"--model": "svm", "--epochs": 1},
 ]
 
 features = [
-    {'--feat': 'os'},
-    # {'--feat': 'os', 
+    {"--feat": "os"},
+    # {'--feat': 'os',
     # '--set': 'ComParE_2016',
     # },
-    # {'--feat': 'mld'},
-    # {'--feat': 'mld',
-    # '--with_os': 'True',
-    # },
-    # {'--feat': 'xbow'},
-    # {'--feat': 'xbow',
-    # '--with_os': 'True',
-    # },
-    # {'--feat': 'trill'},
-    # {'--feat': 'wav2vec'},
+    {"--feat": "audmodel"},
 ]
 
 
 for c in classifiers:
     for f in features:
-        cmd = f'python {src_path}parse_nkulu.py '
+        cmd = "python -m nkululeko.nkuluflag --config exp.ini "
         for item in c:
-            cmd += f'{item} {c[item]} '
+            cmd += f"{item} {c[item]} "
         for item in f:
-            cmd += f'{item} {f[item]} '
+            cmd += f"{item} {f[item]} "
         print(cmd)
         os.system(cmd)
+        # print(f"results: {result}, {last_epoch}")
