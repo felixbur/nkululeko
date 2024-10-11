e6a6f019301a5c6db5422703198603888fb88ddf
Bagus Tris Atmaja <bagustris@yahoo.com>
2024-10-11 17:57:16 +0900

    fix linting for setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 # from nkululeko.constants import VERSION
 from setuptools import setup
 
@@ -6,17 +5,17 @@ setup(
     use_scm_version=True,
     # version=VERSION,
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # 'nkululeko=nkululeko.nkululeko:main',
-            'nkululeko.explore=nkululeko.explore:main',
-            'nkululeko.nkululeko=nkululeko.nkululeko:main',
-            'nkululeko.augment=nkululeko.augment:main',
-            'nkululeko.demo=nkululeko.demo:main',
-            'nkululeko.export=nkululeko.export:main',
-            'nkululeko.predict=nkululeko.predict:main',
-            'nkululeko.resample=nkululeko.resample:main',
-            'nkululeko.segment=nkululeko.segment:main',
-            'nkululeko.test=nkululeko.test:main',
+            "nkululeko.explore=nkululeko.explore:main",
+            "nkululeko.nkululeko=nkululeko.nkululeko:main",
+            "nkululeko.augment=nkululeko.augment:main",
+            "nkululeko.demo=nkululeko.demo:main",
+            "nkululeko.export=nkululeko.export:main",
+            "nkululeko.predict=nkululeko.predict:main",
+            "nkululeko.resample=nkululeko.resample:main",
+            "nkululeko.segment=nkululeko.segment:main",
+            "nkululeko.test=nkululeko.test:main",
         ],
-    }
+    },
 )
